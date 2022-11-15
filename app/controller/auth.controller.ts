d7a2ed6c940f86b24a987c4e4f2584aead97ecbb
Jawad alam <contact.syedjawad@gmail.com>
2022-11-15 11:33:08 +0500

    push

--- app/controller/auth.controller.ts
+++ app/controller/auth.controller.ts
@@ -9,7 +9,53 @@ import { Request, Response, NextFunction } from "express";
     * @apiSuccess {String} account User created Succesfully
     * @apiSuccess {String} session Session logged in database Succesfully
     */
-export const health = async (req: Request,res: Response,next: NextFunction) => {
+export const signUp = async (req: Request, res: Response, next: NextFunction) => {
   throw new Error("Abc")
   res.send('testing');
+};
+/**
+   * @api { post } auth/sign-in Sign In
+   * @apiName Sign In
+   * @apiGroup auth
+   * @apiBody  {String} email Email of the user
+   * @apiBody  {String} password Password of the user
+   * @apiSuccess {String} login User Login Succesfully
+   * @apiSuccess {String} session Session logged in database Succesfully
+   * @apiError  UserNotFound The <code>email or password</code> of the User is not valid.
+   */
+export const signIn = async (req: Request, res: Response, next: NextFunction) => {
+  res.send('signIn');
+};
+/**
+   * @api { post } auth/sign-out Sign Out 
+   * @apiName Sign Out 
+   * @apiGroup auth
+   * @apiSuccess {String} logout User logout Succesfully
+   * @apiSuccess {String} session Session Deleted from database Succesfully
+   * @apiError  UserNotFound User is not logged
+   */
+export const signOut = async (req: Request, res: Response, next: NextFunction) => {
+  res.send('signOut');
+};
+/**
+   * @api { post } auth/forgot-password forgot Password
+   * @apiName forgot Password 
+   * @apiGroup auth
+   * @apiSuccess {String} success password reset link successfully sent
+   * @apiSuccess {String} session Session Deleted from database Succesfully
+   * @apiError  UserNotFound User is not logged
+   */
+export const forgotPassword = async (req: Request, res: Response, next: NextFunction) => {
+  res.send('forgotPassword');
+};
+/**
+   * @api { post } auth/forgot-password forgot Password
+   * @apiName forgot Password 
+   * @apiGroup auth
+   * @apiSuccess {String} success password reset link successfully sent
+   * @apiSuccess {String} session Session Deleted from database Succesfully
+   * @apiError  UserNotFound User is not logged
+   */
+export const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
+  res.send('resetPassword');
 };
--- app/routes/admin.routes.ts
+++ app/routes/admin.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as admin from "../controller/admin.controller";
+import * as admin from "../controller/admin/admin.controller";
 import errorHandler from "../middleware/errorHandller";
 
 const router: Router = Router();
